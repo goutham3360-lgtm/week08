47ba4fcb02bdabf2c40ff54447d4ae7cd791cbd4
Goutham Rao Jonpelli <goutham3360@gmail.com>
2026-09-25 22:27:08 +1000

    Add Terraform CI/CD workflow with remote backend and approval gate

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -5,9 +5,17 @@ terraform {
       version = "~>4.0"
     }
   }
+
+  backend "azurerm" {
+    resource_group_name  = "koalatech-tfstate-rg"
+    storage_account_name = "kttfstate2316609"
+    container_name       = "tfstate"
+    key                  = "week08.terraform.tfstate"
+  }
 }
 
 provider "azurerm" {
   features {}
-  subscription_id = "451d688e-0bac-48e8-95c3-bc25d256b09d"
+  subscription_id                 = "451d688e-0bac-48e8-95c3-bc25d256b09d"
+  resource_provider_registrations = "none"
 }
